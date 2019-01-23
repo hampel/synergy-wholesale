2ef6b30ae6d1d3d1f2e6aabcd1893a7a62d00af1
Simon Hampel <simon@hampelgroup.com>
2019-01-23 14:58:21 +1100

    slight adjustment to Phone type to allow for slightly longer phone numbers, as permitted by the registry

--- src/Types/Phone.php
+++ src/Types/Phone.php
@@ -4,20 +4,24 @@ use SynergyWholesale\Exception\InvalidArgumentException;
 
 class Phone
 {
+	/** @var string */
 	private $phone;
 
 	function __construct($phone)
 	{
 		// strip all spaces
-		$phone = str_replace(' ', '', $phone);
+		$phone = str_replace(' ', '', strval($phone));
 
-		if (!preg_match('/^\+[0-9]{1,2}\.[0-9]{6,10}$/', $phone))
+		if (!preg_match('/^\+[0-9]{1,3}\.[0-9]{6,12}$/', $phone))
 		{
-			throw new InvalidArgumentException("Invalid phone number [{$phone}] - must be in the format +99.999999999");
+			throw new InvalidArgumentException("Invalid phone number [{$phone}] - must be in the format +99.9999999999");
 		}
 		$this->phone = $phone;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function getPhone()
 	{
 		return $this->phone;
